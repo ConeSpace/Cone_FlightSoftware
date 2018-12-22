7ef110be575885ccd6211dfc15d05873acc0b68a
sebastian.dohnal <sebastian.dohnal@a1.net>
2018-12-22 13:23:10 +0100

    Test program added Test Codes added

--- FirstTest/FirstTest/main.cpp
+++ FirstTest/FirstTest/main.cpp
@@ -1,21 +1,41 @@
 #include <wiringPi.h>
 #include <iostream>
+#include <cstdio>
 
-using namespace std;
-// LED-PIN - wiringPi-PIN 0 ist BCM_GPIO 17.
-// Wir müssen bei der Initialisierung mit wiringPiSetupSys die BCM-Nummerierung verwenden.
-// Wenn Sie eine andere PIN-Nummer wählen, verwenden Sie die BCM-Nummerierung, und
-// aktualisieren Sie die Eigenschaftenseiten – Buildereignisse – Remote-Postbuildereignisbefehl 
-// der den GPIO-Export für die Einrichtung für wiringPiSetupSys verwendet.
-#define	LED	17
+/*
+This Script is to provide a testing base for components. It may also be used for debugging.
+
+22-12-2018
+V0.1
+Sebastian Dohnal
+*/
 
 int main(void)
 {
-	//wiringPiSetupSys();
+	//Enter testing loop
+	while (true) {
+		//Ask for Test Code
+		std::cout << "Enter the Test-Code:" << std::endl;
+		int TestCode = 000;
+		std::cin >> TestCode;
+		//Test Code saved as int TestCode
 
-	//pinMode(LED, OUTPUT);
 
-	printf("HELLO WORLD!");
-	
+		//Check if Testcode matches test script and if yes run this script
+
+		if (TestCode == 001) {
+			//Test Script for Test Code 001
+			//Name: Hello World
+			//Desccription: Prints "Hello World" every second
+
+			while (true) {
+				std::cout << "Hello World!" << std::endl;
+				delayMicroseconds(1000000);
+			}
+		} else { 
+			//No matching Test Code found
+			std::cout << "ERROR: Unknown Test-Code" << std::endl;
+		}
+	}
 	return 0;
 }

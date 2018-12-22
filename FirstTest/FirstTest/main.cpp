7c0ee4b8bb52a1864bd9ce76afc4e91ee837910e
sebastian.dohnal <sebastian.dohnal@a1.net>
2018-12-22 12:15:01 +0100

    Hello World Test

--- FirstTest/FirstTest/main.cpp
+++ FirstTest/FirstTest/main.cpp
@@ -1,5 +1,7 @@
 #include <wiringPi.h>
+#include <iostream>
 
+using namespace std;
 // LED-PIN - wiringPi-PIN 0 ist BCM_GPIO 17.
 // Wir müssen bei der Initialisierung mit wiringPiSetupSys die BCM-Nummerierung verwenden.
 // Wenn Sie eine andere PIN-Nummer wählen, verwenden Sie die BCM-Nummerierung, und
@@ -9,16 +11,11 @@
 
 int main(void)
 {
-	wiringPiSetupSys();
+	//wiringPiSetupSys();
 
 	//pinMode(LED, OUTPUT);
 
-	while (true)
-	{
-		/*digitalWrite(LED, HIGH);  // Ein
-		delay(500); // ms
-		digitalWrite(LED, LOW);	  // Aus
-		delay(500);*/
-	}
+	cout << "Hello world\n";
+	
 	return 0;
 }

a4999aebb3b8da1f3e26b7a6b6f380725aeec345
dohnals <45898448+dohnals@users.noreply.github.com>
2019-02-03 23:05:13 +0100

    Just a Backup
    Dunno what I changed

--- FM100.py
+++ FM100.py
@@ -27,7 +27,7 @@ print("QNH: " + str(QnH))
 Comm.fnc_CommTransmit("CFM FM100")
 print ("---FM100 setup done---")
 
-def checkComm():
+'''def checkComm():
     while True:
         time.sleep(1)
         #print("Checking for new Comm msg")
@@ -40,9 +40,116 @@ def checkComm():
                 fm = msgSplit[2]
                 if fm == "FM101":
                     import FM101
+                    
 
-checkComm()
+checkComm()'''
     
 def checkMovement():
     print("Checking if satellite moved")
+    while True:
+        time.sleep(5)
+        try:
+            #---Reading---
+        
+            ACCx, ACCy, ACCz, gyroXangle, gyroYangle, gyroZangle, tiltCompensatedHeading = IMUAccComGyro.fnc_IMU_AxxComGry()
+            heading1 = int(tiltCompensatedHeading)
+            ACCx1 = ACCx
+            ACCy1 = ACCy
+            gryX1 = gyroXangle
+            gryY1 = gyroYangle
+            gryZ1 = gyroZangle
+            #print(heading1)
+            time.sleep(2)
+            ACCx, ACCy, ACCz, gyroXangle, gyroYangle, gyroZangle, tiltCompensatedHeading = IMUAccComGyro.fnc_IMU_AxxComGry()
+            heading2 = int(tiltCompensatedHeading)
+            ACCx2 = ACCx
+            ACCy2 = ACCy
+            gryX2 = gyroXangle
+            gryY2 = gyroYangle
+            gryZ2 = gyroZangle
+            #print(heading2)
+        
+        
+            #print("Checking...")
+        
+            #Heading
+            if heading1 > (heading2 + 2):
+                #print(" heading TURNING!")
+                heading_state = "turning"
+            elif heading2 > (heading1 + 2):
+                #print(" heading TURNING!")
+                heading_state = "turning"
+            else:
+                #print("heading not turning")
+                heading_state = "still"
+            
+        
+            #Gyro X
+            if gryX1 > (gryX2 + 3):
+                gryX_state = "turning"
+                #print("Gyro X Turning!")
+            elif gryX2 > (gryX1 + 3):
+                #print("Gyro X Turning!")
+                gryX_state = "turning"
+            else:
+                #print("Gyro X not turning")
+                gryX_state = "still"
+            
+            #Gyro Y
+            if gryY1 > (gryY2 + 3):
+                #print("Gyro Y Turning!")
+                gryY_state = "turning"
+            elif gryY2 > (gryY1 + 3):
+                #print("Gyro Y Turning!")
+                gryY_state = "turning"
+            else:
+                #print("Gyro Y not turning")
+                gryY_state = "still"
+            
+            #Gyro Z
+            if gryZ1 > (gryZ2 + 0.5):
+                #print("Gyro Z Turning!")
+                gryZ_state = "turning"
+            elif gryZ2 > (gryZ1 + 0.5):
+                #print("Gyro Z Turning!")
+                gryZ_state = "turning"
+            else:
+                #print("Gyro Z not turning")
+                gryZ_state = "still"
+            
+            #find out if satellite is moving or not
+            turning = 0
+            still = 0
+            if heading_state == "turning":
+                turning = turning + 1
+            if gryX_state == "turning":
+                turning = turning + 1
+            if gryY_state == "turning":
+                turning = turning + 1
+            if gryZ_state == "turning":
+                turning = turning + 1
+        
+            if heading_state == "still":
+                still = still + 1
+            if gryX_state == "still":
+                still = still + 1
+            if gryY_state == "still":
+                still = still + 1
+            if gryZ_state == "still":
+                still = still + 1
+            
+            if turning > 1:
+                print("Satellite moving!")
+                satellite_state = "moving"
+                Comm.fnc_CommTransmit("MSG FM100_SatelliteMoving")
+                return
+                break
+        
+            elif still > 3:
+                print("Satellite still!")
+                satellite_state = "still"
+                Comm.fnc_CommTransmit("MSG FM100_SatelliteStill")
     
+        except:
+             pass
+Thread(target = checkMovement).start()

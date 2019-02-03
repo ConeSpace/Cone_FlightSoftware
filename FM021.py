a59eb5e8d224b06d33c02adbb8e8b1b73f79841b
dohnals <45898448+dohnals@users.noreply.github.com>
2019-02-03 15:30:05 +0100

    Fixed FM021

--- FM021.py
+++ FM021.py
@@ -1,8 +1,7 @@
 import Comm
 
 while True:
-    try:
-        x = input('Send: ')
-        Comm.fnc_CommTransmit(str(x))
-    except:
-        pass
+    print("Send:")
+    x = raw_input()
+    print("Transmitted: " + x)
+    Comm.fnc_CommTransmit(str(x))
